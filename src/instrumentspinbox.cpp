dff3a2d31309bbb7a81ca6d57cd6325abe363a37
Vesa Halttunen <vesuri@jormas.com>
2012-02-08 17:49:47 +0200

    Changes: use uppercase chacters in instrument spin box

--- src/instrumentspinbox.cpp
+++ src/instrumentspinbox.cpp
@@ -7,7 +7,7 @@ InstrumentSpinBox::InstrumentSpinBox(QWidget *parent) :
 
 QString InstrumentSpinBox::textFromValue(int value) const
 {
-    return QString("%1").arg(value + 1, 2, 16, QChar('0'));
+    return QString("%1").arg(value + 1, 2, 16, QChar('0')).toUpper();
 }
 
 int InstrumentSpinBox::valueFromText(const QString &text) const
