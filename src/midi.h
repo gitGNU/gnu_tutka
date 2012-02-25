cd046ae9b4114b799ee4756bdaba79cc6c67704a
Vesa Halttunen <vesuri@jormas.com>
2012-02-25 15:43:22 +0200

    Changes: Added copyright information in all files

--- src/midi.h
+++ src/midi.h
@@ -1,22 +1,22 @@
 /*
  * midi.h
  *
- * Copyright 2002-2011 vesuri
+ * Copyright 2002-2012 Vesa Halttunen
  *
- * This file is part of Tutka2.
+ * This file is part of Tutka.
  *
- * Tutka2 is free software; you can redistribute it and/or modify
+ * Tutka is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Tutka2 is distributed in the hope that it will be useful,
+ * Tutka is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Tutka2; if not, write to the Free Software
+ * along with Tutka; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
