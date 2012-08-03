540a6b8eb10a7df69b190d4256abfd79447f19a6
Vesa Halttunen <vesuri@jormas.com>
2012-08-03 13:37:55 +0300

    Changes: Move TODO items to the TODO file
    RevBy: TrustMe

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/*
- * TODO
- *
- * Window positions are not stored
- * Inserting/Deleting a block doesn't update the playing sequence window immediately
- */
-
 #include <QApplication>
 #include <QTranslator>
 #include <QLocale>
